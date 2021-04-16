f08348cf827c5f0cadacdebb72f2b2ad8d5f6b2c
Michael Agustian <michaelagustian9@gmail.com>
2021-04-16 22:46:52 +0700

    adjust UI in movie detail page

--- Movie/Modules/MovieDetail/View/MovieDetailViewController.swift
+++ Movie/Modules/MovieDetail/View/MovieDetailViewController.swift
@@ -19,6 +19,19 @@ final class MovieDetailViewController: UIViewController {
         return playerView
     }()
 
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.isDirectionalLockEnabled = true
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
     private lazy var posterImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "")
@@ -46,6 +59,9 @@ final class MovieDetailViewController: UIViewController {
     private lazy var seeReviewsButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("See Reviews", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 14, weight: .bold)
+        button.backgroundColor = .systemBlue
         button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(self.pushToReviewPage), for: .touchUpInside)
         return button
@@ -89,11 +105,11 @@ final class MovieDetailViewController: UIViewController {
     }
 
     private func setupViews() {
+        setupScrollView()
+
         view.backgroundColor = .systemBackground
         view.addSubview(youTubePlayerView)
-        view.addSubview(posterImageView)
-        view.addSubview(titleLabel)
-        view.addSubview(overviewLabel)
+        view.addSubview(scrollView)
         view.addSubview(seeReviewsButton)
 
         NSLayoutConstraint.activate([
@@ -101,19 +117,42 @@ final class MovieDetailViewController: UIViewController {
             youTubePlayerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             youTubePlayerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             youTubePlayerView.heightAnchor.constraint(equalTo: youTubePlayerView.widthAnchor, multiplier: 9/16),
-            posterImageView.widthAnchor.constraint(equalToConstant: 100),
-            posterImageView.heightAnchor.constraint(equalTo: posterImageView.widthAnchor, multiplier: 3/2),
-            posterImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            posterImageView.topAnchor.constraint(equalTo: youTubePlayerView.bottomAnchor, constant: 16),
-            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
-            titleLabel.topAnchor.constraint(equalTo: posterImageView.bottomAnchor, constant: 16),
-            overviewLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            overviewLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
-            overviewLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: youTubePlayerView.bottomAnchor, constant: 16),
             seeReviewsButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             seeReviewsButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
-            seeReviewsButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
+            seeReviewsButton.topAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 16),
+            seeReviewsButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            seeReviewsButton.heightAnchor.constraint(equalToConstant: 48)
+        ])
+    }
+
+    private func setupScrollView() {
+        setupContainerView()
+        scrollView.addSubview(containerView)
+
+        scrollView.addConstraintsWithFormat("H:|[v0]|", views: containerView)
+        scrollView.addConstraintsWithFormat("V:|[v0]|", views: containerView)
+        containerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+    }
+
+    private func setupContainerView() {
+        containerView.addSubview(posterImageView)
+        containerView.addSubview(titleLabel)
+        containerView.addSubview(overviewLabel)
+
+        NSLayoutConstraint.activate([
+            posterImageView.widthAnchor.constraint(equalToConstant: 100),
+            posterImageView.heightAnchor.constraint(equalTo: posterImageView.widthAnchor, multiplier: 3/2),
+            posterImageView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            posterImageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16),
+            titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: posterImageView.trailingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
+            overviewLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4),
+            overviewLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            overviewLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor)
         ])
     }
 
